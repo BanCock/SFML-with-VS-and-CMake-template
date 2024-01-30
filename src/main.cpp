c54db4daaacdd1283c7fc6364d98555f21e9347a
Максим Шаталов <shatalov.mr@edu.spbstu.ru>
2024-01-31 00:30:31 +0300

    Полностью изменил всё. ImGui и ImGui-SFML теперь внутри проекта, чтобы IntelliSense работал корректно и было удобно. SFML ищется в системе и не устанавливается во время билда. Хочу сделать из этого проекта шаблон, на основе которого в будущем буду делать остальные проекты чтобы не заниматься настройкой каждый раз.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,37 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+#include <imgui.h>
+#include <imgui-SFML.h>
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
 
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::ShowDemoWindow();
+
         window.clear();
+        ImGui::SFML::Render(window);
         window.display();
     }
+
+    ImGui::SFML::Shutdown();
+
+    return 0;
 }
